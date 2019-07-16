c98d104b38458d5ff1ca4797905663c1edd9059f
ahochman <ahochman2019@gmail.com>
2019-07-16 12:51:30 -0400

    some ship math part 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,34 @@
+// frames per second
+const FPS = 30;
+// ship height in pixels
+const SHIP_SIZE = 30; 
+
+
 /** @type {HTMLCanvasElement} */
 var canv = document.getElementById("gameCanvas");
 var ctx = canv.getContext("2d");
+
+var ship = {
+  x: canv.width /2,
+  y: canv.height / 2,
+  r: SHIP_SIZE / 2,
+  A: 90 / 180 * Math.PI // convert to radians
+
+
+}
+
+// set up the game loop
+setInterval(update, 1000 / FPS);
+
+function update() {
+  // draw space
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canv.width, canv.height);
+  //draw ship
+
+
+  //rotate ship
+
+  //move the ship
+
+}
